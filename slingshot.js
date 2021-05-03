f53729e38b5411984c141d0d8f71901d09c607d7
Abinavprogrammer <77102874+Abinavprogrammer@users.noreply.github.com>
2021-05-03 15:22:17 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,22 @@
+class Slingshot{
+
+    constructor(){
+        var Slingshot_options={
+            isStatic : true
+          }
+        
+          this.sling = BodyA=pointB
+          World.add(world,this.ground);
+    }
+    display(){
+        noStroke();
+        fill(188,67,67);
+        rectMode(CENTER);
+        
+fly()
+bodyA=null
+
+
+
+    }
+}
